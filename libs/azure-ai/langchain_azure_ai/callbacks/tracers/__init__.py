95ed0eed41a636b36911a49fac7d9bdd9d2aeb3c
Nagkumar Arkalgud <nagkumar@naarkalg-work-mac.local>
2025-07-31 14:58:05 -0700

    Update imports

--- libs/azure-ai/langchain_azure_ai/callbacks/tracers/__init__.py
+++ libs/azure-ai/langchain_azure_ai/callbacks/tracers/__init__.py
@@ -3,5 +3,8 @@
 from langchain_azure_ai.callbacks.tracers.inference_tracing import (
     AzureAIInferenceTracer,
 )
+from langchain_azure_ai.callbacks.tracers.debug_tracer import (
+    DebugCallbackHandler,
+)
 
-__all__ = ["AzureAIInferenceTracer"]
+__all__ = ["AzureAIInferenceTracer", "DebugCallbackHandler"]
